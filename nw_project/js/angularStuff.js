25216a3ae24ca6112a0c24ac3289112570af862e
Cyrus Roshan <CyrusRoshan@users.noreply.github.com>
2015-10-10 04:02:00 -0500

    google cal parsing

--- nw_project/js/angularStuff.js
+++ nw_project/js/angularStuff.js
@@ -18,4 +18,16 @@ angular.module('angApp', [])
 				warning: false,
 			},
 		]
+
+		//'https://www.google.com/calendar/feeds/i2jntos0susho4i1kf0jl988ls%40group.calendar.google.com/public/basic'
+
+		this.getCal = $.getJSON('http://whateverorigin.org/get?url=' + this.urlValue + '&callback=?', function(data){
+			for(i = 1; i <= data.contents.split("<title type='html'>").length; i++){
+				this.events[i-1].name = data.contents.split("<title type='html'>")[i].split("</title>")[0];
+				this.events[i-1].time = Date.parse(window.stuff.split("<title type='html'>")[i].split("</title>")[1].split("When: ")[1].split("&amp;")[0].split(" to")[0].slice(0, - 2))
+				if(window.stuff.split("<title type='html'>")[5].split("</title>")[1].split("When: ")[1].split("&amp;")[0].split(" to")[0].slice(-2) == 'pm'){
+					this.events[i-1] += 43200000;
+				}
+			}
+		});
 	}]);
---
+++ nw_project/js/doStuff.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	handleAuthClick(event);
+
+	$("#settings").click(function(){
+		//$(".overlayContent").fadeOut("slow");
+		//$(".signupForm").slideUp("slow");
+		//$(".loginForm").slideUp("slow");
+	});
+
+
+});
